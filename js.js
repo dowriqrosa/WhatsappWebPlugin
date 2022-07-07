59f10d1469e43f43508dcb1ab736bd222a7ec239
dowriqrosa <carlosh2204@hotmail.com>
2022-07-07 09:52:28 -0300

    configuração conversas arquivadas

--- js.js
+++ js.js
@@ -7,12 +7,12 @@ window.onload = function () {
 }
 function iniciar() {
     try {
-      if(!funcionou){
+        if(!funcionou){
         alterarDesign();
         funcionou = true;
-      }
+    }
     } catch (error) {
-      
+
     }
 }
 
@@ -24,32 +24,62 @@ function alterarDesign() {
     }
     var clickinicial = ""
     var segundoInicial
-
-    document.querySelector("body").style.width=innerWidth+'px';
     document.querySelector("#side").style.width=innerWidth+'px';
     document.querySelector("#app").style.width=innerWidth+'px';
+    var appClass = document.querySelector("#app").childNodes[0].className.split("app-wrapper-web font-fix");
+    document.querySelector("#app").childNodes[0].className = appClass[0];
     document.querySelector("#app").childNodes[0].style.width=innerWidth+'px';
 
-    document.querySelector("#app").childNodes[0].childNodes[5].childNodes[2].style.display = 'block';
-    document.querySelector("#app").childNodes[0].childNodes[5].childNodes[3].style.display = 'none';
+    telaTodasConversas();
     
     document.querySelector("#app").childNodes[0].childNodes[5].childNodes[2].childNodes[0].childNodes[3].childNodes[2].addEventListener("click", async function() {
-      document.querySelector("#app").childNodes[0].childNodes[5].childNodes[3].style.display = 'block';
-      document.querySelector("#app").childNodes[0].childNodes[5].childNodes[2].style.display = 'none';
-    
-      await sleep(500);
-      document.querySelector("#main").style.width=innerWidth+'px';
-      document.querySelector("#app").childNodes[0].childNodes[5].childNodes[3].childNodes[0].childNodes[4].addEventListener("click",function(e) {
         
-        if(clickinicial == "" || segundoInicial+1000 < Date.now()){
-          clickinicial = e.clientX
-          segundoInicial = Date.now();
-          //&& segundoInicial+1000 > Date.now()
-        }else if(e.clientX < clickinicial-100 ){
-          document.querySelector("#app").childNodes[0].childNodes[5].childNodes[2].style.display = 'block';
-          document.querySelector("#app").childNodes[0].childNodes[5].childNodes[3].style.display = 'none';
-          clickinicial = ""
-        }
-      });
+        telaConversa();
+    
+        await sleep(500);
+        document.querySelector("#main").style.width=innerWidth+'px';
+        document.querySelector("#app").childNodes[0].childNodes[5].childNodes[3].childNodes[0].childNodes[4].addEventListener("click",function(e) {
+        
+            if(clickinicial == "" || segundoInicial+1000 < Date.now()){
+                clickinicial = e.clientX
+                segundoInicial = Date.now();
+            }else if(e.clientX < clickinicial-100 ){
+                telaTodasConversas();
+                clickinicial = ""
+            }
+        });
     });
+
+    //Menu conversas arquivadas
+    document.querySelector("#pane-side > button > div").addEventListener("click", async function(e) {
+        await sleep(500);
+        document.querySelector("#app > div > div").childNodes[1].childNodes[0].childNodes[0].childNodes[0].childNodes[0].childNodes[0].childNodes[1].addEventListener("click", async function(e) {
+            telaConversa();
+
+            await sleep(500);
+            document.querySelector("#main").style.width=innerWidth+'px';
+            document.querySelector("#app").childNodes[0].childNodes[5].childNodes[3].childNodes[0].childNodes[4].addEventListener("click",function(e) {
+            
+                if(clickinicial == "" || segundoInicial+1000 < Date.now()){
+                    clickinicial = e.clientX
+                    segundoInicial = Date.now();
+                }else if(e.clientX < clickinicial-100 ){
+                    telaTodasConversas();
+                    clickinicial = ""
+                }
+            });
+        })
+    });
+
+}
+
+
+function telaConversa() {
+    document.querySelector("#app").childNodes[0].childNodes[5].childNodes[3].style.display = 'block';
+    document.querySelector("#app").childNodes[0].childNodes[5].childNodes[2].style.display = 'none';
+}
+
+function telaTodasConversas() {
+    document.querySelector("#app").childNodes[0].childNodes[5].childNodes[2].style.display = 'block';
+    document.querySelector("#app").childNodes[0].childNodes[5].childNodes[3].style.display = 'none';
 }
